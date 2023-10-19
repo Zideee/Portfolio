65148e6005af4a849d7e9b1e8def575469b485ad
Zideee <Zideee@users.noreply.github.com>
2023-10-19 23:28:48 +0000

    deploy: e8bc759e932c837b14b20bc447aabd4cdf76a74d

--- astro.config.mjs
+++
@@ -1,6 +0,0 @@
-// astro.config.mjs
-import { defineConfig } from 'astro/config'
-
-export default defineConfig({
-  site: 'https://Zideee.github.io', // Remplacez <YOUR_USERNAME> par votre nom d'utilisateur GitHub
-})
